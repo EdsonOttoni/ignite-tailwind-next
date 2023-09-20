84c45ac12dc0186bc246c0617c486eff5a1fbeaa
EdsonOttoni <edson.ottoni@gmail.com>
2023-09-20 11:19:46 -0300

    criando variantes de button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import * as FileInput from '@/components/Form/FileInput'
 import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { Select } from '@/components/Select'
 import { SelectItem } from '@/components/Select/SelectItem'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -21,19 +22,21 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
+            <Button
               className="rounded-lg border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
               type="button"
+              variant="outline"
             >
               Cancel
-            </button>
-            <button
+            </Button>
+            <Button
               className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+              variant="primary"
               type="submit"
               form="setting"
             >
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -217,18 +220,10 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              className="rounded-lg border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-              type="button"
-            >
-              Cancel
-            </button>
-            <button
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-              type="submit"
-            >
-              Save
-            </button>
+            <Button variant="outline" type="button">
+              cancel
+            </Button>
+            <Button type="submit">Save</Button>
           </div>
         </form>
       </div>
